36bd14c59f306d2e678fb02d03f4ebd54ddb5238
chrisbcooper <coopercb@usc.edu>
2021-01-04 23:19:31 -0800

    Updates

---
+++ precache-manifest.533dbe5762fd68b25b316f52b149ce36.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c7a2f85490d3ef6668be8296a12064",
+    "url": "/personalwebsite/index.html"
+  },
+  {
+    "revision": "a475b1a7fa040c2059c7",
+    "url": "/personalwebsite/static/css/main.a47eb7aa.chunk.css"
+  },
+  {
+    "revision": "d5b8b6ff46a9cbb608e4",
+    "url": "/personalwebsite/static/js/2.e3b9264c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personalwebsite/static/js/2.e3b9264c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a475b1a7fa040c2059c7",
+    "url": "/personalwebsite/static/js/main.d9ca9941.chunk.js"
+  },
+  {
+    "revision": "88cc35093163ff61ff0a",
+    "url": "/personalwebsite/static/js/runtime-main.4600424b.js"
+  },
+  {
+    "revision": "d8b5090dc7748526fa59e9e63b4479e8",
+    "url": "/personalwebsite/static/media/ArcadeHoops.d8b5090d.png"
+  },
+  {
+    "revision": "946d4eb2fa3a007dab6de514d1816659",
+    "url": "/personalwebsite/static/media/HandsUp.946d4eb2.jpeg"
+  },
+  {
+    "revision": "657c5a60b18cff10316a266c6f49ad1a",
+    "url": "/personalwebsite/static/media/QuickWork.657c5a60.jpeg"
+  },
+  {
+    "revision": "c9496154eeda92a48cea143c10a29b43",
+    "url": "/personalwebsite/static/media/Resume.c9496154.pdf"
+  },
+  {
+    "revision": "c7e35bb5e5c9a73a3893244da2c67f38",
+    "url": "/personalwebsite/static/media/SpotifyInsider.c7e35bb5.png"
+  },
+  {
+    "revision": "e365989c1ff2f3e258af3fd7b6f7b945",
+    "url": "/personalwebsite/static/media/profile-pic.e365989c.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf540d49924a23972cae9ba88a0141a4.js"
+  "/personalwebsite/precache-manifest.533dbe5762fd68b25b316f52b149ce36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personalwebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
