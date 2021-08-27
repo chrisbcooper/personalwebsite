d0162a81ac71a6c52366a455d6bf3298141cd295
chrisbcooper <coopercb@usc.edu>
2021-08-26 17:05:36 -0700

    Make small changes to website

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -3,36 +3,35 @@ import EducationPage from './EducationPage';
 import ExperiencePage from './ExperiencePage';
 
 function Experience() {
-
-  return (
-    <div id='experience' className='font'>
-    <h1>Experience</h1>
-    <div className='experience-container'>
-    <ExperiencePage
-      position='Incoming Software Engineering Intern'
-      time='Starting June 2021'
-      name='PIMCO'
-      />
-      <div style={{height: 40}}  />
-      <ExperiencePage 
-        position='Software Engineering Intern' 
-        time='December 2020 - Present'
-        name='Inked Sports'
-        description="Inked Sports is a student run startup 
+	return (
+		<div id='experience' className='font'>
+			<h1>Experience</h1>
+			<div className='experience-container'>
+				<ExperiencePage
+					position='Software Engineering Intern'
+					time='June 2021 - August 2021'
+					name='PIMCO'
+				/>
+				<div style={{ height: 40 }} />
+				<ExperiencePage
+					position='Software Engineering Intern'
+					time='December 2020 - August 2021'
+					name='Inked Sports'
+					description='Inked Sports is a student run startup 
         that looks to use Social Media to help college athletes
         with their personal branding. The app is going to use 
         the Instagram API to receive and analyze the data from
         their social media, and use that to help them get deals
         with companies. The app has a Firebase backend, and a 
-        React Native frontend."
-      />
-    </div>
-      <h1>Education</h1>
-      <div className='experience-container'>
-      <EducationPage />
-      </div>
-    </div>
-  );
+        React Native frontend.'
+				/>
+			</div>
+			<h1>Education</h1>
+			<div className='experience-container'>
+				<EducationPage />
+			</div>
+		</div>
+	);
 }
 
 export default Experience;
--- src/components/Pages/AboutMe.js
+++ src/components/Pages/AboutMe.js
@@ -2,51 +2,60 @@ import React from 'react';
 import profilePic from '../../img/Me/profile-pic.jpeg';
 
 function AboutMe() {
+	const Text = () => (
+		<div className='aboutme-words'>
+			<div className='aboutme-description'>
+				<p>
+					Hi! I'm Christopher, and I'm currently a sophomore studying
+					Computer Science at The University of Southern California
+				</p>
+				<p></p>
+				<p>
+					{' '}
+					I am passionate about Software Engineering, and I am
+					constantly trying to learn more and grow.{' '}
+				</p>
+				<p></p>
+				<p>
+					When I'm not coding I'm usually playing basketball or just
+					spending time with friends and family. I am currently
+					looking for a Software Engineering Internship for Summer
+					2022.
+				</p>
+				<p></p>
+				<p>Here are some technologies that I enjoy using:</p>
+				<div className='inner-grid'>
+					<div className='aboutme-list'>C++</div>
+					<div className='aboutme-list'>Java</div>
+					<div className='aboutme-list'>Python</div>
+					<div className='aboutme-list'>Node</div>
+					<div className='aboutme-list'>Nodejs</div>
+					<div className='aboutme-list'>MongoDB</div>
+					<div className='aboutme-list'>SQL</div>
+					<div className='aboutme-list'>Reactjs</div>
+					<div className='aboutme-list'>HTML/CSS/JS</div>
+					<div className='aboutme-list'>Git</div>
+				</div>
+			</div>
+		</div>
+	);
 
-  const Text = () => (
-    <div className='aboutme-words'>
-        <div className='aboutme-description' >
-        <p>Hi! I'm Christopher, and I'm currently a sophomore studying Computer Science at The University of Southern California</p>
-          <p></p>
-          <p> I am passionate about
-          Software Engineering, and I am constantly trying to learn more and grow. </p>
-          <p></p>
-          <p>
-          When I'm not coding I'm usually playing basketball or just spending time with friends and family.
-          I am currently looking for a Software Engineering Internship
-          for Summer 2021.
-          </p>
-          <p></p>
-          <p>Here are some technologies that I enjoy using:</p>
-          <div className="inner-grid">
-          <div className='aboutme-list'>C++</div>
-          <div className='aboutme-list'>Java</div>
-          <div className='aboutme-list'>Swift</div>
-          <div className='aboutme-list'>HTML/CSS/JS</div>
-          <div className='aboutme-list'>Reactjs</div>
-          <div className='aboutme-list'>Nodejs</div>
-          </div>
-          </div>
-          </div>
-  );
+	const Im = () => (
+		<div>
+			<span className='helper'></span>
+			<img src={profilePic} alt='' className='profile-pic' />
+		</div>
+	);
 
-  const Im = () => (
-    <div>
-          <span className='helper'></span>
-          <img src={profilePic} alt=''  className='profile-pic' />
-          </div>
-  );
-
-
-  return (
-    <div className='font' id='aboutme'>
-      <h1>About me</h1>
-        <div className='aboutme-grid'>
-          <Im />
-          <Text />
-        </div>
-    </div>
-  );
+	return (
+		<div className='font' id='aboutme'>
+			<h1>About me</h1>
+			<div className='aboutme-grid'>
+				<Im />
+				<Text />
+			</div>
+		</div>
+	);
 }
 
 export default AboutMe;
